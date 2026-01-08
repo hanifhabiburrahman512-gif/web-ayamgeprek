87ef4ebf0877f628b0074d46e7a5ab41bb36d9f1
hanif <hanifhabiburrahman512@upi.edu>
2026-01-08 18:13:36 +0700

    ayam3

--- api/index.php
+++ api/index.php
@@ -1,2 +1,26 @@
 <?php
+
+// --- KONFIGURASI KHUSUS VERCEL ---
+// Karena Vercel bersifat Read-Only, kita harus memindahkan 
+// semua file cache yang mau ditulis Laravel ke folder /tmp
+
+$tmpPath = '/tmp';
+
+// Set path untuk cache file sistem Laravel
+putenv("APP_PACKAGES_CACHE={$tmpPath}/packages.php");
+putenv("APP_SERVICES_CACHE={$tmpPath}/services.php");
+putenv("APP_CONFIG_CACHE={$tmpPath}/config.php");
+putenv("APP_ROUTES_CACHE={$tmpPath}/routes.php");
+putenv("APP_EVENTS_CACHE={$tmpPath}/events.php");
+
+// Set path untuk view yang di-compile (Blade)
+putenv("VIEW_COMPILED_PATH={$tmpPath}/views");
+
+// Pastikan folder view cache ada
+if (!is_dir($tmpPath . '/views')) {
+    mkdir($tmpPath . '/views', 0755, true);
+}
+
+// --- JALANKAN LARAVEL ---
+// Panggil file utama Laravel dari folder public
 require __DIR__ . '/../public/index.php';
